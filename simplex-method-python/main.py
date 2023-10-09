bec9704c3c8869dce20391d2d470331884ac4dda
Aleksandr Ryabov <a.ryabov@innopolis.university>
2023-10-09 17:31:24 +0300

    Finished algoritmic part

--- simplex-method-python/main.py
+++ simplex-method-python/main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import itertools as itls
 
+# Input data
+maximization = True
 approx = 0.001
 
 C = np.array([
@@ -21,28 +23,90 @@ b = np.array([
 # Save shape of array 
 n, m = C.shape
 
+# Step 0
 # Find basic feasible solution
 for seq in itls.permutations(((m - n) * [0] + n * [1]), m):
 
     # Generate index sequence for array shaping
-    true_seq = [i for i in range(0, len(seq)) if seq[i] == 1]
-    anti_seq = [i for i in range(0, len(seq)) if seq[i] == 0]
+    basic_seq = [i for i in range(0, len(seq)) if seq[i] == 1]
+    non_basic_seq = [i for i in range(0, len(seq)) if seq[i] == 0]
 
-    A_tmp = A[true_seq]
-    C_tmp = C[:, true_seq]
+    print(basic_seq, non_basic_seq)
 
-    # Check linear dependency
-    lambdas, V = np.linalg.eig(C_tmp.T)
-    # print(C_tmp[lambdas == 0, :])
-    if len(C_tmp[lambdas == 0, :]) != 0 :
+    A_tmp = A[basic_seq]
+    C_tmp = C[:, basic_seq]
+
+    # Check linear dependency 
+    # using eigenvalue decomposition 
+    lambdas, V = np.linalg.eig(C[:, basic_seq].T)
+    # print(C[:, basic_seq][lambdas == 0, :])
+    if len(C[:, basic_seq][lambdas == 0, :]) != 0 :
         continue
     else:
-        X_b = C_tmp @ b # np.transpose may be needed
-        # z = A_tmp @ X_b
+        break 
+
+while True:
+    # Step 1 & 2
+    # Compute inverse and z - c
+    # Check optimality of the solution
+
+    # Count X_b
+    X_b = np.linalg.inv(C[:, basic_seq]) @ b # np.transpose may be needed
+    # print("X_b ", X_b)
+
+    # Count z - c        
+    Z_c = A[basic_seq] @ np.linalg.inv(C[:, basic_seq]) @ C[:, non_basic_seq] - A[non_basic_seq]
+
+    # Count z
+    z = A[basic_seq] @ X_b
+
+    # Cheking optimality
+
+    # Maximization optimal
+    if (maximization and all(i > 0 for i in Z_c)):
+        print("### Maximum achived ###")
+        break
+
+    # Minimazation optimal
+    elif (not maximization and all(i <= 0 for i in Z_c)):
+        print("### Minimum achived ###")
+        break
         
-        Z_c = A_tmp @ C_tmp @ C[:, anti_seq] - A[anti_seq]
-        
-        exit()
+    # Min/Max not optimal
+    else:
+        pass
+
+    # Determine entering variable (vector)
+    enter_var_ind = 0
+    if maximization:
+        enter_var_ind = np.argmin(Z_c)
+    else: 
+        enter_var_ind = np.argmax(Z_c)
+
+
+    # Step 3
+    # Check solition bound
+    # Determine leaving variable (vector)
+
+    B_p = np.linalg.inv(C[:, basic_seq]) @ C[:, enter_var_ind]
+
+    # Compute slope cooficient
+    k = np.divide(X_b, B_p)
+    k = [i if (i >= 0 and i != float('inf')) else float('inf') for i in k]
+
+    # Determine leaving value
+    leaving_var_ind = np.argmin(k)
+
+
+    # Step 4
+    # Form new basis
+
+    # Update state of the basic and non-basic vectors
+    non_basic_seq[enter_var_ind], basic_seq[leaving_var_ind] = basic_seq[leaving_var_ind], non_basic_seq[enter_var_ind]
+
+    print(basic_seq)
+
+
 
 
 
